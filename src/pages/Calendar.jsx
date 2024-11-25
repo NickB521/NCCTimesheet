7e96d7117eb12ba9c62d17eb95d457430eec03c2
Benbow <benrocks2005@gmail.com>
2024-11-25 13:43:04 -0500

    fixed rerendering states

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -2,40 +2,111 @@ import React, { useState, useCallback } from "react";
 import {
     Button, Card, CardHeader, CardBody, Table,
     TableHeader, TableBody, TableRow, TableColumn, TableCell,
-    Textarea, DatePicker
+    Textarea, DatePicker, Tooltip, Checkbox
 } from "@nextui-org/react";
-import {CalendarDate, getDayOfWeek } from "@internationalized/date";
+import { CalendarDate, getDayOfWeek } from "@internationalized/date";
 
 const Calendar = () => {
     const [week, setWeek] = useState({
-        monday: "",
-        tuesday: "",
-        wednesday: "",
-        thursday: "",
-        friday: "",
-        saturday: "",
-        sunday: ""
+        monday: {
+            day: "",
+            startTime: "",
+            endTime: "",
+            break: {
+                taken: false,
+                start: "",
+                end: ""
+            }
+        },
+        tuesday: {
+            day: "",
+            startTime: "",
+            endTime: "",
+            break: {
+                taken: false,
+                start: "",
+                end: ""
+            }
+        },
+        wednesday: {
+            day: "",
+            startTime: "",
+            endTime: "",
+            break: {
+                taken: false,
+                start: "",
+                end: ""
+            }
+        },
+        thursday: {
+            day: "",
+            startTime: "",
+            endTime: "",
+            break: {
+                taken: false,
+                start: "",
+                end: ""
+            }
+        },
+        friday: {
+            day: "",
+            startTime: "",
+            endTime: "",
+            break: {
+                taken: false,
+                start: "",
+                end: ""
+            }
+        },
+        saturday: {
+            day: "",
+            startTime: "",
+            endTime: "",
+            break: {
+                taken: false,
+                start: "",
+                end: ""
+            }
+        },
+        sunday: {
+            day: "",
+            startTime: "",
+            endTime: "",
+            break: {
+                taken: false,
+                start: "",
+                end: ""
+            }
+        }
     });
 
     const stuff = (input) => {
         const date = new CalendarDate(input.year, input.month, input.day);
+        const key = Object.keys(week)
         if (getDayOfWeek(date, "en-US") == 1) {
-            for (const [key, value] of Object.entries(week)) {
-                if (key != "key") {
-                    week.key = input.month + "/" + input.day
-                    console.log(key)
-                    document.getElementById(key).innerHTML = week.key
-                    input.day += 1
-                }
+            for (let i = 0; i < key.length; i++) {
+                week[key[i]].day = input.month + "/" + input.day
+                document.getElementById(key[i]).innerHTML = week[key[i]].day + ""
+                input.day += 1
             }
             input.day -= 7
-            console.log(input)
             setWeek(week);
             document.getElementById("errorCode").innerHTML = "";
         } else {
             document.getElementById("errorCode").innerHTML = "Please select a Monday!";
+            if (document.getElementById("monday").innerHTML.includes("/")) {
+                for (let i = 0; i < key.length; i++) {
+                    document.getElementById(key[i]).innerHTML = ""
+                }
+            }
         }
+    }
 
+    const breaked = (input) => {
+        const key = input
+        setWeek(week => ({
+            ...week, [key]: {...week[key], break: {...week[key].break, taken: !(week[key].break.taken)}}
+        }));
     }
 
     return (
@@ -72,7 +143,41 @@ const Calendar = () => {
                                 <TableRow>
                                     <TableCell>I am User Man</TableCell>
                                     <TableCell>
-                                        <Button></Button>
+                                        <Tooltip
+                                            showArrow
+                                            placement="bottom"
+                                            content={
+                                                <>
+                                                    <h1>Monday Shift</h1>
+                                                    <Textarea label={"Start Time"}></Textarea>
+                                                    {
+                                                    // week.monday.break.taken ?
+                                                        // <Checkbox id="Break" onClick={() => breaked("monday")} defaultSelected>Break</Checkbox>
+                                                        // :
+                                                        <Checkbox onClick={() => breaked("monday")} isSelected={week.monday.break.taken}>Break</Checkbox>
+                                                    }
+                                                    {week.monday.break.taken ?
+                                                        <>
+                                                            <Textarea label={"Break Start"}></Textarea>
+                                                            <Textarea label={"Break End"}></Textarea>
+                                                        </>
+                                                        : <></>}
+                                                    <Textarea label={"End Time"}></Textarea>
+                                                </>
+                                            }
+                                            classNames={{
+                                                base: [
+                                                    // arrow color
+                                                    "before:bg-neutral-400 dark:before:bg-white",
+                                                ],
+                                                content: [
+                                                    "py-2 px-4 shadow-xl",
+                                                    "text-black bg-gradient-to-br from-white to-neutral-400",
+                                                ],
+                                            }}
+                                        >
+                                            <Button></Button>
+                                        </Tooltip>
                                     </TableCell>
                                     <TableCell>
                                         <Button></Button>
