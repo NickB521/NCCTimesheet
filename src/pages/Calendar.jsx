1770140a9778b21aafc5f0ae38e273e4f8eaae0b
Benbow <benrocks2005@gmail.com>
2024-12-02 18:44:13 -0500

    Added feature for start time to persist and update the state

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,82 +1,166 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import {
     Button, Card, CardHeader, CardBody, Table,
     TableHeader, TableBody, TableRow, TableColumn, TableCell,
-    Textarea, DatePicker, Tooltip, Checkbox
+    Textarea, DatePicker, Tooltip, Checkbox, TimeInput
 } from "@nextui-org/react";
-import { CalendarDate, getDayOfWeek } from "@internationalized/date";
+import { CalendarDate, getDayOfWeek, Time } from "@internationalized/date";
+
+const WeekTool = ({ week, stuffed, breaked, day }) => {
+    const handleSubmit = (inpt, timeType) => {
+        // console.log(Math.round((inpt.minute / 15) * 15));
+        console.log(inpt)
+        setWeek(week => ({
+            ...week, [day]: { ...week[day], [timeType]: { ...week[day][timeType], hour: inpt.hour, minute: ((((inpt.minute + 7.5)/15 | 0) * 15) % 60), second: inpt.second }}
+        }));
+    }
+    return (
+        <>
+            <Tooltip
+                showArrow
+                placement="bottom"
+                content={
+                    // Make the form handle all input
+                    // change the provider to allow for native handling or adapt to aria????
+                    <>
+                        <h1>Shift</h1>
+                        {/* Make the onchange change the value of the start time and set value to starttime value for controlled input???? */}
+                        <TimeInput isRequired id={`StartTime-${day}`} label={"Start Time"} 
+                            onChange={(inpt) => stuffed(inpt, day, "startTime")} value={week[day].startTime}></TimeInput>
+                        <Checkbox onClick={() => breaked(day)} isSelected={week[day].break.taken}>Meal Break?</Checkbox>
+                        {week[day].break.taken ?
+                            <>
+                                <Textarea label={"Break Start"}></Textarea>
+                                <Textarea label={"Break End"}></Textarea>
+                            </>
+                            : <></>}
+                        <Textarea id={`EndTime-${day}`} label={"End Time"}></Textarea>
+                        <Button type="submit">Finish</Button>
+                    </>
+                }
+                classNames={{
+                    base: [
+                        // arrow color
+                        "before:bg-neutral-400 dark:before:bg-white",
+                    ],
+                    content: [
+                        "py-2 px-4 shadow-xl",
+                        "text-black bg-gradient-to-br from-white to-neutral-400",
+                    ],
+                }}
+            >
+                <Button></Button>
+            </Tooltip>
+        </>
+    );
+}
 
 const Calendar = () => {
     const [week, setWeek] = useState({
         monday: {
             day: "",
-            startTime: "",
+            startTime: {
+                hour: 0,
+                minute: 0,
+                second: 0
+            },
             endTime: "",
             break: {
                 taken: false,
                 start: "",
                 end: ""
-            }
+            },
+            submitted: false
         },
         tuesday: {
             day: "",
-            startTime: "",
+            startTime: {
+                hour: 0,
+                minute: 0,
+                second: 0
+            },
             endTime: "",
             break: {
                 taken: false,
                 start: "",
                 end: ""
-            }
+            },
+            submitted: false
         },
         wednesday: {
             day: "",
-            startTime: "",
+            startTime: {
+                hour: 0,
+                minute: 0,
+                second: 0
+            },
             endTime: "",
             break: {
                 taken: false,
                 start: "",
                 end: ""
-            }
+            },
+            submitted: false
         },
         thursday: {
             day: "",
-            startTime: "",
+            startTime: {
+                hour: 0,
+                minute: 0,
+                second: 0
+            },
             endTime: "",
             break: {
                 taken: false,
                 start: "",
                 end: ""
-            }
+            },
+            submitted: false
         },
         friday: {
             day: "",
-            startTime: "",
+            startTime: {
+                hour: 0,
+                minute: 0,
+                second: 0
+            },
             endTime: "",
             break: {
                 taken: false,
                 start: "",
                 end: ""
-            }
+            },
+            submitted: false
         },
         saturday: {
             day: "",
-            startTime: "",
+            startTime: {
+                hour: 0,
+                minute: 0,
+                second: 0
+            },
             endTime: "",
             break: {
                 taken: false,
                 start: "",
                 end: ""
-            }
+            },
+            submitted: false
         },
         sunday: {
             day: "",
-            startTime: "",
+            startTime: {
+                hour: 0,
+                minute: 0,
+                second: 0
+            },
             endTime: "",
             break: {
                 taken: false,
                 start: "",
                 end: ""
-            }
+            },
+            submitted: false
         }
     });
 
@@ -102,10 +186,15 @@ const Calendar = () => {
         }
     }
 
-    const breaked = (input) => {
-        const key = input
+    const breaked = (day) => {
         setWeek(week => ({
-            ...week, [key]: {...week[key], break: {...week[key].break, taken: !(week[key].break.taken)}}
+            ...week, [day]: { ...week[day], break: { ...week[day].break, taken: !(week[day].break.taken) } }
+        }));
+    }
+
+    const timeSet = (inpt, day, timeType) => {
+        setWeek(week => ({
+            ...week, [day]: { ...week[day], [timeType]: { ...week[day][timeType], hour: inpt.hour, minute: ((((inpt.minute + 7.5)/15 | 0) * 15) % 60), second: inpt.second }}
         }));
     }
 
@@ -143,59 +232,25 @@ const Calendar = () => {
                                 <TableRow>
                                     <TableCell>I am User Man</TableCell>
                                     <TableCell>
-                                        <Tooltip
-                                            showArrow
-                                            placement="bottom"
-                                            content={
-                                                <>
-                                                    <h1>Monday Shift</h1>
-                                                    <Textarea label={"Start Time"}></Textarea>
-                                                    {
-                                                    // week.monday.break.taken ?
-                                                        // <Checkbox id="Break" onClick={() => breaked("monday")} defaultSelected>Break</Checkbox>
-                                                        // :
-                                                        <Checkbox onClick={() => breaked("monday")} isSelected={week.monday.break.taken}>Break</Checkbox>
-                                                    }
-                                                    {week.monday.break.taken ?
-                                                        <>
-                                                            <Textarea label={"Break Start"}></Textarea>
-                                                            <Textarea label={"Break End"}></Textarea>
-                                                        </>
-                                                        : <></>}
-                                                    <Textarea label={"End Time"}></Textarea>
-                                                </>
-                                            }
-                                            classNames={{
-                                                base: [
-                                                    // arrow color
-                                                    "before:bg-neutral-400 dark:before:bg-white",
-                                                ],
-                                                content: [
-                                                    "py-2 px-4 shadow-xl",
-                                                    "text-black bg-gradient-to-br from-white to-neutral-400",
-                                                ],
-                                            }}
-                                        >
-                                            <Button></Button>
-                                        </Tooltip>
+                                        <WeekTool week={week} stuffed={timeSet} breaked={breaked} day={"monday"} />
                                     </TableCell>
                                     <TableCell>
-                                        <Button></Button>
+                                        <WeekTool week={week} stuffed={timeSet} breaked={breaked} day={"tuesday"} />
                                     </TableCell>
                                     <TableCell>
-                                        <Button></Button>
+                                        <WeekTool week={week} stuffed={timeSet} breaked={breaked} day={"wednesday"} />
                                     </TableCell>
                                     <TableCell>
-                                        <Button></Button>
+                                        <WeekTool week={week} stuffed={timeSet} breaked={breaked} day={"thursday"} />
                                     </TableCell>
                                     <TableCell>
-                                        <Button></Button>
+                                        <WeekTool week={week} stuffed={timeSet} breaked={breaked} day={"friday"} />
                                     </TableCell>
                                     <TableCell>
-                                        <Button></Button>
+                                        <WeekTool week={week} stuffed={timeSet} breaked={breaked} day={"saturday"} />
                                     </TableCell>
                                     <TableCell>
-                                        <Button></Button>
+                                        <WeekTool week={week} stuffed={timeSet} breaked={breaked} day={"sunday"} />
                                     </TableCell>
                                     <TableCell>
                                         <Textarea></Textarea>
