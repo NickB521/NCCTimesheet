abde5758d9649cb90a7b9b6605ff370cb8f6d2d4
Jahmai88 <jahmaijohnson8@gmail.com>
2024-12-02 13:07:00 -0500

    Holiday Icons

--- src/assets/icons/dashboard/index.js
+++ src/assets/icons/dashboard/index.js
@@ -1,3 +1,21 @@
+import exp from "constants"
+
 export * from "./seperator"
 export * from "./edit"
 export * from "./forward"
+export * from "./thanksgiving"
+export * from "./newyears"
+export * from "./martinlutherking"
+export * from "./christmas"
+export * from "./labor"
+export * from "./juneteenth"
+export * from "./veterans"
+export * from "./colombus"
+export * from "./independence"
+export * from "./memorial"
+export * from "./washingtonbirthday"
+
+
+
+
+
