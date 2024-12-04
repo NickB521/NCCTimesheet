a89979aae01afe7befffd6ac536c73f407683aef
Jahmai88 <jahmaijohnson8@gmail.com>
2024-12-04 12:50:43 -0500

    Update dashboard-data.jsx

---
+++ src/assets/data/dashboard-data.jsx
@@ -0,0 +1,7 @@
+import {christmas, colombus, inaguration, independence, juneteenth, labor, martinlutherking, memorial, newyears, seperator, thanksgiving, veterans, washingtonbirthday } from "../icons/dashboard"
+
+export const items = [
+    {
+
+    }
+]
