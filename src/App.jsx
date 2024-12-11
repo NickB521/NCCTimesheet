e3fd32f50c7b00650033d2155c763a6efe0d4e91
WEasterday <weasterday001@gmail.com>
2024-12-11 14:52:29 -0500

    setting up sign in validation
    it is being set up here is the push! yayyy

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ const App = () => {
 
   const [user, setUser] = useState({
     name: "USER IS ME",
-    role: "Supervisor",
+    role: "Coordinator",
     email: "email@email.com",
     worksite: {
       name: ""
