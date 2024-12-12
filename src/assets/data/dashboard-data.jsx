d996dc3f68c231db3ef1df37498aabc89fd52aa8
Jahmai88 <jahmaijohnson8@gmail.com>
2024-12-12 14:51:13 -0500

    commitment

--- src/assets/data/dashboard-data.jsx
+++ src/assets/data/dashboard-data.jsx
@@ -2,6 +2,54 @@ import {christmas, colombus, inaguration, independence, juneteenth, labor, marti
 
 export const items = [
     {
+        label: "New Years",
+        icon: <newyears/>,
+        description: "Jan 1st"
+    },
+    {
+        label: "Martin Luther King",
+        icon: <martinlutherking/>,
+        description: "Jan 15th"
+    },
+    {
+        label: "Washingtons Birthday",
+        icon: <washingtonbirthday/>,
+        description: "Feb 19th"
 
+    },
+    {
+        label: "Memorial Day",
+        icon: <memorial/>,
+        description: "May 27th"
+    },
+    {
+        label: "Juneteenth",
+        icon: <juneteenth/>,
+        description: "Jun 19th"
+    },
+    {
+        label: "Independence Day",
+        icon: <independence/>,
+        description: "Jul 4th"
+    },
+    {
+        label: "Labor Day",
+        icon: <labor/>,
+        description: "Sep 2nd"
+    },
+    {
+        label: "Veterans Day",
+        icon: <veterans/>,
+        description: "Nov 11th"
+    },
+    {
+        label: "Thanksgiving",
+        icon: <thanksgiving/>,
+        description: "Nov 28th"
+    },
+    {
+        label: "Christmas",
+        icon: <christmas/>,
+        description: "Dec 25th"
     }
 ]
